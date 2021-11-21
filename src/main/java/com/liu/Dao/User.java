a6bf90cd8491d7cfc308e854c8d273f078502b66
Reue <429838880@qq.com>
2021-11-21 16:40:21 +0800

    默认变更列表

--- src/main/java/com/liu/Dao/User.java
+++ src/main/java/com/liu/Dao/User.java
@@ -1,9 +1,13 @@
 package com.liu.Dao;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     int id;
     String user;
